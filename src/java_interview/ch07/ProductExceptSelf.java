220b9f34bb207817bf0f9f7edf19cbf1120ca408
JunRain <sa2313@tukorea.ac.kr>
2023-12-29 18:00:05 +0900

    자신을 제외한 배열의 곱

---
+++ src/java_interview/ch07/ProductExceptSelf.java
@@ -0,0 +1,28 @@
+package java_interview.ch07;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 나눗셈 이용 금지 -> 배열 각 인덱스에 전체 곱을 넣은 후 각 인덱스로 나누는 방법 금지
+ */
+public class ProductExceptSelf {
+	public int[] productExceptSelf(int[] nums) {
+		int[] result = new int[nums.length];
+
+		// 왼쪽 곱셈
+		int p = 1;
+		for (int i = 0; i < nums.length; i++) {
+			result[i] = p;
+			p *= nums[i];
+		}
+
+		// 오른쪽 곱셈
+		p = 1;
+		for (int i = nums.length - 1; i >= 0; i--) {
+			result[i] = p;
+			p *= nums[i];
+		}
+		return result;
+	}
+}
