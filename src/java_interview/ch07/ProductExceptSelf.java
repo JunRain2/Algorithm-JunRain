b4d76e70bfb67f31115b8d3ff42ae3997d45910e
JunRain <sa2313@tukorea.ac.kr>
2023-12-30 12:55:11 +0900

    자신을 제외한 배열의 곱/개념

--- src/java_interview/ch07/ProductExceptSelf.java
+++ src/java_interview/ch07/ProductExceptSelf.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 /**
  * 나눗셈 이용 금지 -> 배열 각 인덱스에 전체 곱을 넣은 후 각 인덱스로 나누는 방법 금지
+ * 자기자신을 제외하고 왼쪽의 곱셈 결과와 오른쪽의 곱셈 결과를 곱하는 방법
+ * 왼 -> 오 : 곱셈을 쌓아서 감
+ * 오 -> 왼 : 왼 -> 오 에서 쌓이지 못한 곱셈을 쌓아줌
  */
 public class ProductExceptSelf {
 	public int[] productExceptSelf(int[] nums) {
@@ -13,6 +16,7 @@ public class ProductExceptSelf {
 		// 왼쪽 곱셈
 		int p = 1;
 		for (int i = 0; i < nums.length; i++) {
+			// 대입을 먼저해서 자기자신을 제외시킴
 			result[i] = p;
 			p *= nums[i];
 		}
@@ -20,7 +24,7 @@ public class ProductExceptSelf {
 		// 오른쪽 곱셈
 		p = 1;
 		for (int i = nums.length - 1; i >= 0; i--) {
-			result[i] = p;
+			result[i] *= p;
 			p *= nums[i];
 		}
 		return result;
