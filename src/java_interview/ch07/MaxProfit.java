26dd7eefd0f31756161bd542e2f21a678118caa3
JunRain <sa2313@tukorea.ac.kr>
2024-01-01 23:47:05 +0900

    주식을 사고팔기 가장 좋은시점 - timeout

---
+++ src/java_interview/ch07/MaxProfit.java
@@ -0,0 +1,14 @@
+package java_interview.ch07;
+
+public class MaxProfit {
+	public int maxProfit(int[] prices) {
+		int max = 0;
+		for (int i = 0; i < prices.length; i++) {
+			for (int j = i + 1; j < prices.length; j++) {
+				max = Math.max(max, prices[j] - prices[i]);
+			}
+		}
+		return max;
+	}
+}
+
