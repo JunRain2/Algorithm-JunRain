333c3e3a9f234adb80c6bf2b09e01d35036451a7
JunRain <sa2313@tukorea.ac.kr>
2024-01-03 02:22:48 +0900

    두 수의 덧셈

---
+++ src/java_interview/ch08/AddTwoSumNumbers.java
@@ -0,0 +1,91 @@
+package java_interview.ch08;
+
+import java.math.BigInteger;
+
+public class AddTwoSumNumbers {
+	public ListNode reverseList(ListNode head) {
+		ListNode prev = null, node = head;
+		while (node != null) {
+			ListNode next = node.next;
+			node.next = prev;
+			prev = node;
+			node = next;
+		}
+		return prev;
+	}
+
+	// 연결 리스트를 임의 정밀도 정수형으로 변환
+	public BigInteger toBigInt(ListNode node) {
+		String val = "";
+		while (node != null) {
+			val += node.val;
+			node = node.next;
+		}
+		return new BigInteger(val);
+	}
+
+	// 임의 정밀도 정수형을 연결 리스트로 변환
+	public ListNode toReversedLinkedList(BigInteger val) {
+		ListNode prev = null, node = null;
+		for (char c : String.valueOf(val).toCharArray()) {
+			// 한 글자씩 숫자로 변환하여 노드 지정
+			// 숫자 형태의 char형을 int형으로 변환
+			node = new ListNode(Character.getNumericValue(c));
+			node.next = prev;
+			prev = node;
+		}
+		return node;
+	}
+
+	// 우아한 방식이 아닌 난잡한 방식
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+		// 두 리스트를 뒤집는다.
+		ListNode l1Reversed = reverseList(l1);
+		ListNode l2Reversed = reverseList(l2);
+
+		// 두 리스트를 임의 정밀도 정수형으로 변환하여 더하기 연산을 진행한다.
+		BigInteger result = toBigInt(l1Reversed).add(toBigInt(l2Reversed));
+		// 결과를 다시 역순 연결 리스트로 변환한다.
+		return toReversedLinkedList(result);
+	}
+
+	// 전가산기 구현
+
+	/**
+	 * 자리 올림수(carry)를 구하는 것까지 가산기의 원리와 거의 동일
+	 * 덧셈 결과에 나머지를 취하고 몫은 자리올림수 형태로 올리는 전가산기의 전체적인 구조만 참고
+	 */
+	public ListNode addTwoNumberFullAdd(ListNode l1, ListNode l2) {
+		// 값을 계산할 임시 노드
+		ListNode node = new ListNode(0);
+		// 임시 노드를 첫 번째 노드로 선언
+		ListNode root = node;
+
+		// 자릿수의 합 (sum), 자리올림수(carry), 나머지(remainder)를 저장할 변수 선언
+		int sum, carry = 0, remainder;
+		// 모든 연결 리스트를 끝까지 순회하고, 자리올림수도 0이 될 때까지 진행
+		while (l1 != null || l2 != null || carry != 0) {
+			sum = 0;
+			if (l1 != null) {
+				sum += l1.val;
+				l1 = l1.next;
+			}
+			if (l2 != null) {
+				sum += l2.val;
+				l2 = l2.next;
+			}
+
+			// 노드의 값으로 사용할 나머지 계산
+			remainder = (sum + carry) % 10;
+			// 10으로 나누었을 때 몫은 자릿수가 증가했다는 의미이므로 자리올림수로 사용
+			carry = (sum + carry) / 10;
+			// 나머지는 다음 노드의 값으로 지정
+			node.next = new ListNode(remainder);
+			// 계산할 노드를 다음으로 이동
+			node = node.next;
+		}
+
+		// 첫 번째 노드는 임시 노드이므로 그다음부터 결과로 리턴
+		return root.next;
+	}
+}
--- src/java_interview/ch08/ReverseList.java
+++ src/java_interview/ch08/ReverseList.java
@@ -2,6 +2,8 @@ package java_interview.ch08;
 
 public class ReverseList {
 	// 재귀
+	// 다음 노드 next와 현재 노드 node를 파라미터로 하여 계속해서 재귀 호출
+	// node.next에는 이전 prev 리스트를 계속 연결해주면서 node가 null이 될 때까지 재귀 호출하면 마지막에는 백트래킹되면서 연결 리스트가 거꾸로 연결
 	// 맨 처음에 리턴된 prev는 뒤집힌 연결 리스트의 첫 번째 노드가 된다.
 	public ListNode reverse(ListNode node, ListNode prev) {
 		// 현재 노드인 node가 null이면 리턴
@@ -13,6 +15,7 @@ public class ReverseList {
 		// 현재 노드의 다음으로 이전 노드 지정
 		node.next = prev;
 		// 다음 노드와 현재 노드를 피라미터로 하여 재귀 호출
+		// node.next = prev 된 노드가 기입
 		return reverse(next, node);
 	}
 
