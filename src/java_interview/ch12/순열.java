999479a553d5a12556a8f83675d504a861fb789a
JunRain <sa2313@tukorea.ac.kr>
2024-01-24 04:12:58 +0900

    순열

--- src/java_interview/ch12/순열.java
+++ src/java_interview/ch12/순열.java
@@ -1,2 +1,36 @@
-package java_interview.ch12;public class 순열 {
+package java_interview.ch12;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class 순열 {
+	public List<List<Integer>> permute(int[] nums) {
+		List<Integer> numList = new ArrayList<>();
+		for (int n : nums) {
+			numList.add(n);
+		}
+
+		List<List<Integer>> result = new ArrayList<>();
+
+		dfs(result, numList, new ArrayList<>());
+
+		return result;
+	}
+
+	public void dfs(List<List<Integer>> result, List<Integer> subList, List<Integer> path) {
+		if (subList.size() == 0) {
+			result.add(path);
+			return;
+		}
+
+		for (int i = 0; i < subList.size(); i++) {
+			List<Integer> pathTmp = new ArrayList<Integer>(path);
+			List<Integer> subListTmp = new ArrayList<Integer>(subList);
+			pathTmp.add(subList.get(i));
+			subListTmp.remove(i);
+			dfs(result, subListTmp, pathTmp);
+		}
+	}
+
 }
+// https://leetcode.com/problems/permutations/description/
