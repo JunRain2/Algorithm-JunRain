4cc48d4750e16012cedd874f9e43f8719ef80742
JunRain <sa2313@tukorea.ac.kr>
2024-01-25 01:20:46 +0900

    조합의 합-책 풀이

--- src/java_interview/ch12/조합의_합.java
+++ src/java_interview/ch12/조합의_합.java
@@ -1,5 +1,6 @@
 package java_interview.ch12;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -35,6 +36,35 @@ public class 조합의_합 {
 	/**
 	 * 내가 푼 풀이
 	 * 처음에는 중복을 생각하지 못해서 풀지 못했다.
+	 */
+	public List<List<Integer>> combinationSumBook(int[] candidates, int target) {
+		List<List<Integer>> results = new ArrayList<>();
+
+		dfsBook(results, candidates, target, 0, new ArrayDeque<Integer>());
+
+		return results;
+	}
+
+	// sum 은 현재의 합
+	public void dfsBook(List<List<Integer>> results, int[] candidates, int target, int index,
+		ArrayDeque<Integer> path) {
+		if (target == 0) {
+			results.add(new ArrayList<>(path));
+			return;
+		}
+
+		if (target < 0) {
+			return;
+		}
+
+		for (int i = index; i < candidates.length; i++) {
+			path.add(candidates[i]);
+			dfsBook(results, candidates, target - candidates[i], i, path);
+			path.removeLast();
+		}
+	}
+	/**
+	 * target에서 candidate[i]를 빼는 방식으로 불필요한 매개 변수를 줄였다.
 	 */
 }
 // https://leetcode.com/problems/combination-sum/
