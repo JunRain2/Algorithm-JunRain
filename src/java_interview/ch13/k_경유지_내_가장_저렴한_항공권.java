740e23e9db470c08fd4c43ccd6fea8588ceb08b9
JunRain <sa2313@tukorea.ac.kr>
2024-05-08 02:30:37 +0900

    네트워크 딜레마 타임 복습

--- src/java_interview/ch13/k_경유지_내_가장_저렴한_항공권.java
+++ src/java_interview/ch13/k_경유지_내_가장_저렴한_항공권.java
@@ -1,8 +1,49 @@
 package java_interview.ch13;
 
+import java.util.AbstractMap;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class k_경유지_내_가장_저렴한_항공권 {
+	// n : 노드의 수, flights : 그래프, src : 출발지, dst : 도착지, k개의 경유지 이내
 	public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
 		
 	}
+
+	public int networkDelayTime(int[][] times, int n, int k) {
+		Map<Integer, Map<Integer, Integer>> graph = new HashMap<>();
+		for (int[] time : times) {
+			graph.putIfAbsent(time[0], new HashMap<>());
+			graph.get(time[0]).put(time[1], time[2]);
+		}
+
+		Queue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>(Map.Entry.comparingByValue());
+		pq.add(new AbstractMap.SimpleEntry<>(k, 0));
+		Map<Integer, Integer> dist = new HashMap<>();
+		while (!pq.isEmpty()) {
+			Map.Entry<Integer, Integer> cur = pq.poll();
+			int u = cur.getKey();
+			int dist_u = cur.getValue();
+
+			if(!dist.containsKey(u)) {
+				dist.put(u, dist_u);
+				if(graph.containsKey(u)) {
+				for(Map.Entry<Integer, Integer> v : graph.get(u).entrySet()) {
+					int alt = dist_u + v.getValue();
+					pq.add(new AbstractMap.SimpleEntry<>(v.getKey(), alt));
+				}
+			}
+		}
+	}
+		if (dist.size() == n) {
+			return Collections.max(dist.values());
+		}
+		return -1;
+		}
+
+
 }
 // https://leetcode.com/problems/cheapest-flights-within-k-stops/description/
--- src/java_interview/ch13/네트워크_딜레이_타임.java
+++ src/java_interview/ch13/네트워크_딜레이_타임.java
@@ -8,8 +8,15 @@ import java.util.PriorityQueue;
 import java.util.Queue;
 
 /**
- * 트리에서 같은 깊이를 모두 탐색해서 pq에 담는다.
- * 깊이 상관없이 pq에서 가장 작은 값을 골라서, 트리를 수색하면서 값이 제일 적게 드는 경로를 탐색한다.
+ * dist에 값이 없는 경우
+ * 1. pq 에서 거리가 제일 작은 노드A를 꺼낸다.
+ * 2. 해당 노드A를 dist에 넣는다.
+ * 3. 출발지가 노드 A인 값을 graph에서 찾는다.
+ * 4. 출발지에서 노드 A까지의 거리를 graph에서 찾은 노드의 거리에 합한 후 pq에 넣는다. -> 출발지를 기준으로 제일 작은 거리를 재는 것이기 때문
+ * 5. 1. 부터 반복한다.
+ *
+ * dist에 값이 있는 경우
+ *    dist에 있는 노드의 거리가 가장 짧기 때문에 그냥 넘어간다.
  */
 public class 네트워크_딜레이_타임 {
 	/**
@@ -19,7 +26,7 @@ public class 네트워크_딜레이_타임 {
 	 * 		만약 노드가 8개인데 다익스트라 알고리즘 계산은 7개밖에 할 수 없다면, 나머지 한 노드에는 도달할 수 없다는 의미
 	 */
 	public int networkDelayTime(int[][] times, int n, int k) {
-		// 네트워크의 각 노드를 출발지 -> 도착지 형태의 그래프로 구성
+		// key(출발지) : value(key(도착지) : value(비용))
 		Map<Integer, Map<Integer, Integer>> graph = new HashMap<>();
 		for (int[] time : times) {
 			// 값이 존재하지 않을 경우 빈 해시맵 생성
@@ -27,8 +34,8 @@ public class 네트워크_딜레이_타임 {
 			// 출발지에 (도착지, 소요 시간) 추가
 			graph.get(time[0]).put(time[1], time[2]);
 		}
-		// 우선순위 큐 생성, 값이 (도착지, 소요 시간)으로 구성되므로
-		// 정렬 기준으로 도착지(key)와 소요 시간(value) 중 소요 시간을 기준으로 한다.
+		// 우선순위 큐 생성, 값이 graph의 value(도착지, 소요 시간)으로 구성되므로
+		// 정렬 기준으로 도착지(key)와 소요 시간(value) 중 소요 시간을 기준으로 한다. -> 짧은 경로를 계산하는 것이기 때문
 		// Map이 아닌 Map.Entry를 사용 한 이유
 		// -> 맵은 기본적으로 목록형(List)이기 때문에 값을 조회하려면 항상 for 문으로 순회하여 추출하는 번거로운 과정이 필요
 		// -> Map.Entry는 항상 1개의 아이템을 의미하므로 getKey(), getValue() 메소드로 순회 과정 없이 즉시 값을 추출할 수 있다.
@@ -40,6 +47,7 @@ public class 네트워크_딜레이_타임 {
 		Map<Integer, Integer> dist = new HashMap<>();
 		// 큐에 값이 남아 있다면 없어질 때까지 반복
 		// pq를 기준으로 탐색하기 때문에, 방문했던 노드를 기준으로 최소 거리를 찾아갈 수 있음.
+		// 메인 로직
 		while (!pq.isEmpty()) {
 			// 소요 시간이 가장 빠른 값 추출 -> value가 가장 작은 값부터 추출
 			Map.Entry<Integer, Integer> cur = pq.poll();
@@ -54,9 +62,11 @@ public class 네트워크_딜레이_타임 {
 				dist.put(u, dist_u);
 				// u 지점을 출발지로 한 경로가 있다면 처리 시작
 				if (graph.containsKey(u)) {
-					// u 지점을 출발지로 한 모든 경로 순회
+					// u 지점을 출발지로 한 모든 경로 순회, v는 u의 도착지들
+					// pq의 영역을 넓히는 부분 -> 즉 dist에 넣은 도착지를 출발지로 하여 pq에 저장하는 과정
 					for (Map.Entry<Integer, Integer> v : graph.get(u).entrySet()) {
 						// u 지점까지의 소요 시간(dist_u) + u 지점을 출발지로 한 도착까지의 소요 시간
+						// 출발지로부터 최단거리만 골라서 도착한 소요 시간
 						int alt = dist_u + v.getValue();
 						// 우선순위 큐에 (도착지, 소요 시간)을 삽입
 						pq.add(new AbstractMap.SimpleEntry<>(v.getKey(), alt));
