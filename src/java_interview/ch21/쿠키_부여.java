2dfc1dc3342ec936c0da0fe807fc82f9c53326e7
JunRain <sa2313@tukorea.ac.kr>
2024-12-09 21:48:45 +0900

    findContentChildren/그리디 알고리즘

--- src/java_interview/ch21/쿠키_부여.java
+++ src/java_interview/ch21/쿠키_부여.java
@@ -1,28 +1,24 @@
 package java_interview.ch21;
 
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
 
 public class 쿠키_부여 {
 	public int findContentChildren(int[] g, int[] s) {
-		int result = 0;
+		// 그리디하게 처리하기 위해 입력값 모두 정렬
+		Arrays.sort(g);
+		Arrays.sort(s);
 
-		// 어린아이들과 쿠키를 역순으로 정렬
-		Integer[] children = Arrays.stream(g).boxed().toArray(Integer[]::new);
-		Arrays.sort(children, Collections.reverseOrder());
-		Integer[] cookies = Arrays.stream(s).boxed().toArray(Integer[]::new);
-		Arrays.sort(cookies, Comparator.reverseOrder());
+		int i = 0; // 어린이를 가리키는 인덱스
+		int j = 0; // 쿠키를 가리키는 인덱스
 
-		int i = 0;
-		for (Integer c : cookies) {
-			for (; i < children.length; i++) {
-				if (c >= children[i]) {
-					result++;
-				}
+		// 어린이 인덱스와 쿠키 인덱스가 각각 전체 길이보다 작을 때 반복
+		while (i < g.length && j < s.length) {
+			// 어린이가 원하는 크기보다 쿠키가 더 클 때만 조건이 만족되고 다음 어린이로 이동
+			if (s[j] >= g[i]) {
+				i++;
 			}
+			j++; // 다음 쿠키로 이동
 		}
-
-		return result;
+		return i;
 	}
 }
