1b78cc7557b939fbf4707ba11af8412224cfb9cd
JunRain <sa2313@tukorea.ac.kr>
2024-12-11 19:53:19 +0900

    majorityElement/풀이[분할 정복]

--- src/java_interview/ch22/과반수_엘리먼트.java
+++ src/java_interview/ch22/과반수_엘리먼트.java
@@ -1,32 +1,29 @@
 package java_interview.ch22;
 
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 public class 과반수_엘리먼트 {
 	public int majorityElement(int[] nums) {
-		// 엘리먼트 개수 계산
-		Map<Integer, Integer> countsMap = new HashMap<>();
-		for (int num : nums) {
-			countsMap.put(num, countsMap.getOrDefault(num, 0) + 1);
-		}
-		// 내림차순 정렬을 저장할 맵은 입력 순서가 유지되는 LinkedHashMap으로 선언 -> 역순으로 정렬한 맵을 입려 순서 그대로 유지해야 하기 때문
-		Map<Integer, Integer> reverseSortedMap = new LinkedHashMap<>();
-		// countsMap을 내림차순으로 정렬해 reverseSortedMap으로 저장
-		countsMap.entrySet()
-			.stream()
-			.sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())) // 값 기준, 역순 정렬 처리
-			.forEachOrdered(x -> reverseSortedMap.put(x.getKey(), x.getValue()));
+		return majorityElement(0, nums.length - 1, nums);
+	}
 
-		// 첫 번째 항목 추출
-		Map.Entry<Integer, Integer> entry = reverseSortedMap.entrySet().iterator().next();
-		// 과반수를 넘을 경우 정답으로 리턴
-		if (entry.getValue() > nums.length / 2) {
-			return entry.getKey();
+	public int majorityElement(int left, int right, int nums[]) {
+		// 최소 분할 단위 리턴
+		if (left == right) {
+			return nums[left];
 		}
-		// 항상 정답이 있는 문제이기 때문에 앞에서 처리되지만, 자바에서 리턴이 없으면 경고가 발생하므로 처리
-		return -1;
+
+		// 중앙 위치 인덱스 계산
+		int mid = left + (right - left) / 2;
+		int a = majorityElement(left, mid, nums);
+		int b = majorityElement(mid + 1, right, nums);
+
+		int countA = 0;
+		// 해당 구간의 a 개수 계산
+		for (int i = left; i <= right; i++) {
+			if (nums[i] == a) {
+				countA++;
+			}
+		}
+		// a가 과반수를 넘으면 a 리턴
+		return countA > (right - left + 1) / 2 ? a : b;
 	}
 }
